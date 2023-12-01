cc999a7b5b7395f1179c7c2cd95402dacca5c54d
IronIdk <jmsfo@iscte-iul.pt>
2023-12-01 21:08:23 +0000

    fe: Added a countdown section

--- scripts.js
+++ scripts.js
@@ -100,3 +100,31 @@ function generateTeamMembers() {
 
 // Call the function to generate team member cards
 generateTeamMembers();
+
+// Add the following function to calculate the countdown
+function updateCountdown() {
+    const launchDate = new Date('June 1, 2024 00:00:00 GMT+0000').getTime();
+    const currentDate = new Date().getTime();
+    const timeDifference = launchDate - currentDate;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById('days').textContent = formatTime(days);
+    document.getElementById('hours').textContent = formatTime(hours);
+    document.getElementById('minutes').textContent = formatTime(minutes);
+    document.getElementById('seconds').textContent = formatTime(seconds);
+}
+
+// Helper function to format time (add leading zero)
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// Call the updateCountdown function
+updateCountdown();
+
+// Update the countdown every second
+setInterval(updateCountdown, 1000);
