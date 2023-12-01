a1b7c2d7e854a402a393be83ee392f396179789c
IronIdk <jmsfo@iscte-iul.pt>
2023-12-01 14:39:03 +0000

    Update the corporate site

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
-function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const mainContent = document.querySelector('.main-content');
-    sidebar.style.width = sidebar.style.width === '250px' ? '0' : '250px';
-    mainContent.style.marginLeft = mainContent.style.marginLeft === '250px' ? '0' : '250px';
+// Function to remove the loading screen and show the main content
+function removeLoadingScreen() {
+    const loadingScreen = document.querySelector('.loading-screen');
+    const body = document.body;
+
+    // Add the 'hide' class to trigger the fade-out effect
+    loadingScreen.classList.add('hide');
+
+    // Remove the 'loading' class from the body to show the scrollbar
+    body.classList.remove('loading');
+
+    // Set a timeout to remove the loading screen after the fade-out animation completes
+    setTimeout(() => {
+        loadingScreen.style.display = 'none';
+    }, 1000); // Adjust the timeout based on your transition duration
 }
+
+// Set a timeout to remove the loading screen after 4 seconds
+setTimeout(removeLoadingScreen, 2500);
